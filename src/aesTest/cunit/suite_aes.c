d5cae8d8f5f1d0086fe99429d41ee3e5efd463b8
Riku Kalinen <riku@kalinen.net>
2017-03-15 12:45:10 +0200

    test toolchain working

--- src/aesTest/cunit/suite_aes.c
+++ src/aesTest/cunit/suite_aes.c
@@ -10,11 +10,22 @@ int suite_clean(void) {
     return 0;
 }
 
+/* dummy tests to just check the toolchain is working. */
+
+/* always successful */
+
 void test_void(void) {
   CU_ASSERT(0 == 0);
+}
+
+/* always fails */
+
+void test_fail(void) {
+  CU_ASSERT(1 == 0);
 }
 
 void gradle_cunit_register() {
     CU_pSuite pSuiteRypto = CU_add_suite("rypto tests", suite_init, suite_clean);
     CU_add_test(pSuiteRypto, "test_void", test_void);
+    /* CU_add_test(pSuiteRypto, "test_fail", test_fail); */
 }

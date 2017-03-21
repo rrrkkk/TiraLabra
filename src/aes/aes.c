437500efeee5e102f5ab91cafd48256ac117eaf0
Riku Kalinen <riku@kalinen.net>
2017-03-21 12:46:22 +0200

    KeyExpansion intermediate

--- src/aes/aes.c
+++ src/aes/aes.c
@@ -30,7 +30,7 @@ const AES_byte AES_S_Box[256] = {
    this is for 128 bit keys.
  */
 
-AES_word AES_Rcon[10] = {
+const AES_word AES_Rcon[10] = {
   0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000,
   0x20000000, 0x40000000, 0x80000000, 0x1b000000, 0x36000000
 };
